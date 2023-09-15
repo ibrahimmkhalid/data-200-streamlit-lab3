c62ab76414a7e61d8d0d22218cf97265354de9ef
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2023-09-14 20:32:18 -0700

    update

--- lab.py
+++ lab.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 st.write("Testing publishing with streamlit")
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4, 5],
-  'second column': [10, 20, 30, 40, 50]
+  'first column': [1, 2, 3, 4, 5, 6],
+  'second column': [10, 20, 30, 40, 50, 60]
 })
 
 st.write(df)
